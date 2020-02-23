a8d7543299e22d7bffe936e68905fa29414a31df
ShilpaAshok100 <shilpashreea1994@gmail.com>
2020-02-23 16:12:17 +0530

    added packages and classes

---
+++ src/main/java/com/qa/libs/Libs.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class Libs {
+	
+	public void libs() {
+		System.out.println("Libraries");
+	}
+
+}
---
+++ src/main/java/com/qa/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.qa.utils;
+
+public class Utils {
+	
+	public void utils() {
+		System.out.println("Utils");
+	}
+
+}
